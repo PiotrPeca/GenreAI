4c3d9511cae51f86f6735b404a6a604c9e16e8f8
Piotr Peca <piotrpeca@gmail.com>
2026-01-11 20:20:43 +0100

    drag&drop added and new UI

--- app/public/script.js
+++ app/public/script.js
@@ -1,6 +1,5 @@
 // ============= NOTE HOVER ANIMATION =============
-
-const noteIcon = document.getElementById('note-icon');
+const noteIcon = document.querySelector('#note-icon');
 
 noteIcon.addEventListener('mouseover', () => {
     noteIcon.classList.add('fa-shake');
@@ -10,46 +9,72 @@ noteIcon.addEventListener('mouseout', () => {
     noteIcon.classList.remove('fa-shake');
 })
 
-// ============= UPLOAD TO BACKEND =============
-const uploadForm = document.getElementById('upload-form');
-const audioFileInput = document.getElementById('audio-file');
-const uploadContent = document.getElementById('upload-content');
-const loader = document.getElementById('loader-animation');
-const resultDiv = document.getElementById('result');
+// ============= DRAG AND DROP =============
+const dropArea = document.querySelector("#drop-area");
+const inputFile = document.querySelector("#input-file");
+const folderIcon = document.querySelector("#folder-icon");
+const loader = document.querySelector("#loader-animation");
+const genreResult = document.querySelector("#result");
 
-uploadForm.addEventListener('submit', (e) => {
+dropArea.addEventListener('dragover', (e) => {
     e.preventDefault();
+    dropArea.classList.add('dragging');
+    folderIcon.classList.replace('fa-folder', 'fa-folder-open');
 });
 
-audioFileInput.addEventListener('change', (e) => {
-    const file = e.target.files[0];
+dropArea.addEventListener('dragleave', (e) => {
+    dropArea.classList.remove('dragging');
+    folderIcon.classList.replace('fa-folder-open', 'fa-folder');
+});
 
-    uploadContent.style.display = 'none';
-    loader.style.display = 'block';
+dropArea.addEventListener('drop', (e) => {
+    e.preventDefault();
+    dropArea.classList.remove('dragging');
+    folderIcon.classList.replace('fa-folder-open', 'fa-folder');
+    const file = e.dataTransfer.files[0];
+    uploadFile(file);
+})
 
-    if (file) {
-        uploadFile(file);
-    }
+dropArea.addEventListener('mouseover', () => {
+    dropArea.classList.add('dragging');
+    folderIcon.classList.replace('fa-folder', 'fa-folder-open');
+})
+
+dropArea.addEventListener('mouseout', () => {
+    dropArea.classList.remove('dragging');
+    folderIcon.classList.replace('fa-folder-open', 'fa-folder');
+})
+
+inputFile.addEventListener('change', () => {
+    const file = inputFile.files[0];
+
+    uploadFile(file);
 });
 
 function uploadFile(file) {
+    dropArea.style.display = 'none';
+    loader.style.display = 'block';
+    genreResult.style.display = 'none';
+
     const formData = new FormData();
-    formData.append('audioFile', file);
+    formData.append("audioFile", file);
     
     fetch('/api/upload', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.json())
+    .then(res => res.json())
     .then(data => {
         loader.style.display = 'none';
-        uploadContent.style.display = 'flex';
-        resultDiv.textContent = `Genre: ${data.genre}`;
-        resultDiv.style.display = 'flex';
+        dropArea.style.display = 'flex';
+        genreResult.style.display = 'flex';
+        genreResult.textContent = `Genre: ${data.genre}`;
     })
     .catch(error => {
         loader.style.display = 'none';
-        uploadContent.style.display = 'flex';
+        dropArea.style.display = 'flex';
+        genreResult.style.display = 'flex';
+        genreResult.textContent = "Sorry, an error occured!";
         console.error('Error:', error);
     });
 }
